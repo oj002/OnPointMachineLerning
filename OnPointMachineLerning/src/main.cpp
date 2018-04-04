34725bd30f0149c207c154c7417b11d277eadb91
Olaf B <olaf2@iris-volker.de>
2018-04-04 09:56:31 +0200

    res.zip added

--- OnPointMachineLerning/src/main.cpp
+++ OnPointMachineLerning/src/main.cpp
@@ -3,6 +3,13 @@
 
 #define _OPML_ENABLE_OPENCV
 
+/*
+	Git canot push files larger than 100 mb
+	if they are bigger than 50 mb there will be a warning
+
+	Therefore only publish with binary's or large resources in an release
+*/
+
 int main()
 {
 	opml::Examples::Mnist_NN mnist(200);
